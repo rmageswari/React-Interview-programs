ad14edd9f5c0df471030b998f3fabe73cc014734
rmageswari <79086585+rmageswari@users.noreply.github.com>
2022-10-10 07:58:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React-Interview-programs
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-sgbbjx)
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./style.css";
+
+export default function App() {
+
+
+  const handleDateDiff = () => {
+
+  }
+  
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <button onClick={() => handleDateDiff()}>Date Diff</button>
+    </div>
+  );
+}
